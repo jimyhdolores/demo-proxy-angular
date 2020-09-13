1a1c8d727463206941421f49361d6cc6618cd808
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2020-09-13 15:04:03 -0500

    config init

--- src/app/apirest.service.ts
+++ src/app/apirest.service.ts
@@ -8,7 +8,6 @@ import { Observable } from 'rxjs';
 export class ApiRestService {
   constructor(private http: HttpClient) { }
 
-  private headers = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
   getGreeting(): Observable<any> {
     // return this.http.get<any>('http://localhost:8080/apidemo/greeting');
     return this.http.get<any>('apidemo/greeting');
